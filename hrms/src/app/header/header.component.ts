002ae03bbe7644c1d8b88d0e6e3bc23a704d7669
Gargi Vatturkar <vgargiu@gmail.com>
2021-10-08 02:46:47 -0700

    top header + configurations

---
+++ hrms/src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  currentTime;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.currentTime = new Date().getHours() + " : " +  new Date().getMinutes() + " : " + new Date().getSeconds() + " IST";
+  }
+
+}
